d3b0588c7b78c51f3db5f60464f96c633090a57a
szymon <szymciem8@gmail.com>
2021-12-09 10:51:00 +0100

    update

--- scripts/autonomous_docking.py
+++ scripts/autonomous_docking.py
@@ -11,9 +11,13 @@ import matplotlib.pyplot as plt
 from subprocess import call, Popen
 import time
 
+import signal
+
 # CONSTANTS
 OFFSET = 250
 MIN_ERROR = 10
+
+global_stop_flag = False
 
 xavier_setup = 'export ROS_MASTER_URI=http://192.168.1.101:11311 && export ROS_IP=192.168.1.112'
 
@@ -22,12 +26,11 @@ left_wheel_publisher = rospy.Publisher('left_velocity/command', Float64, queue_s
 
 rospy.init_node('robot_controller', anonymous=True)
 
+#Setpoint [rad/s]
 #W miarę OK
 Kp = 14
 Ki = 7
 Kd = 0.3
-
-#Setpoint [rad/s]
 
 # RIGHT WHEEL PID
 pid_right = PID(Kp, Ki, Kd, setpoint=0)
@@ -40,10 +43,9 @@ pid_left.sample_time = 0.01
 pid_left.output_limits = (-1000, 1000)
 
 # ALIGNMENT PID
-# pid_align = PID(1.5, 0.2, 0.008) # było 0.5, 0.1, 0.0
-pid_align = PID(0.5, 0.003, 0.007)
+pid_align = PID(1.5, 0.2, 0.008)
 pid_align.sample_time = 0.01
-pid_align.setpoint=0
+pid_align.setpoint = 0
 pid_align.output_limits = (-15, 15)
 
 # Read values from topic
@@ -59,6 +61,10 @@ frame_size = 10
 average_tfmini = [[0 for i in range(frame_size)] for _ in range(4)]
 
 def precise_tfmini_measurement():
+    '''
+    Process data from tfmini sensors using a moving average filter.
+    '''
+    
     precise_tfmini = [0, 0, 0, 0]
 
     for i in range(4):
@@ -80,7 +86,11 @@ def controller_manager_setup():
     command += ' && rosrun controller_manager controller_manager spawn right_velocity'
     command += ' && rosrun controller_manager controller_manager spawn left_velocity'
     p1=Popen(['/bin/bash', '-i', '-c', command])
-    p1.wait()
+
+    p1.wait(timeout=60)
+
+    while not global_stop_flag: pass
+    p1.terminate()
 
 def callback_pololu(msg, i):
     global pololu_measurements
@@ -99,13 +109,21 @@ def callback_joint_state(msg):
     # print(right_wheel_speed, left_wheel_speed)
 
 def listener():
+    global global_stop_flag
+
     rospy.Subscriber('/joint_states', JointState, callback_joint_state)
     for i in range(4):
         rospy.Subscriber('/mega_driver/pololu/scan_'+str(i), Range, callback_pololu, callback_args=i)
         rospy.Subscriber('/mega_driver/tfmini/scan_'+str(i), Range, callback_tfmini, callback_args=i)
-    rospy.spin()
+
+    while not global_stop_flag: pass
+    # rospy.spin()
 
 def move_right_wheel(speed):
+    '''
+    Move right wheel using PID controller.
+    '''
+
     global pid_right, right_wheel_speed
 
     pid_right.setpoint=speed
@@ -117,6 +135,10 @@ def move_right_wheel(speed):
     right_wheel_publisher.publish(output)
 
 def move_left_wheel(speed):
+    '''
+    Move left wheel using PID controller.
+    '''
+
     global pid_left, left_wheel_speed 
     pid_left.setpoint=speed
     output = pid_left(left_wheel_speed)
@@ -127,10 +149,18 @@ def move_left_wheel(speed):
     left_wheel_publisher.publish(output)
 
 def full_stop():
+    '''
+    Stop all motors, by sending 0% PWM signal. 
+    '''
+
     right_wheel_publisher.publish(0)
     left_wheel_publisher.publish(0)
 
 def align_robot(sensor='tfmini', precision=False):
+    '''
+    Put robot in parallel position to the wall. 
+    '''
+
     global pid_align
 
     if sensor=='tfmini':
@@ -146,42 +176,66 @@ def align_robot(sensor='tfmini', precision=False):
         error = pololu_measurements[3] - pololu_measurements[2] 
         if -MIN_ERROR <= error <= MIN_ERROR: error = 0
 
-   
+    
     # print('tf2', tfmini_measurements[2], end='\t')
     # print('tf3', tfmini_measurements[3], end='\t')
 
     pid_align.setpoint=0
     output_align = pid_align(error)
 
-    # if output_align > 0: output_align += 10
-    # elif output_align <0: output_align -= 10
-
     # move_right_wheel(output_align)
 
     print(f'{output_align = }, {error=}')
 
-    return output_align
+    return output_align, error
 
+def find_docking_wall():
+    '''
+    Based on the distance of the front lidar, specify if the robot
+    is close enough to the wall to start the alignment process. 
+    '''
 
+    pass
+
+def docking():
+    pass 
+
+def signal_handler(signal, frame):
+    global global_stop_flag
+    global_stop_flag = True
+
+    
 if __name__ == '__main__':
 
     #Use right_velocity and left_velocity instead of diff_drive
     #Disconnect built in PID controller
-    controller_manager_setup()
+    # controller_manager_setup()
+
+    signal.signal(signal.SIGINT, signal_handler)
 
     #Start listener thread
     listener_thread = threading.Thread(target=listener)
+    listener_thread.setDaemon(True)
     listener_thread.start()
 
-    try:
-        while True:
-            # move_right_wheel(5+align_robot())
-            # move_left_wheel(5)
-            # time.sleep(0.01)
-            full_stop()
-            #right_wheel_publisher.publish(0)
-            #precise_tfmini_measurement()
-    
-    except KeyboardInterrupt:
+    while True:
+        alignment_movement, alignment_error = align_robot()
+        try:
+            forward_movement = 5 / abs(alignment_error)
+        except ZeroDivisionError:
+            forward_movement = 5
+        # move_left_wheel(forward_movement - alignment_movement)
+        # move_right_wheel(forward_movement)
+        # align_robot()
         full_stop()
-        listener_thread.join()
+        # right_wheel_publisher.publish(0)
+        # precise_tfmini_measurement()
+
+        if global_stop_flag:
+            break
+
+    print('debug message')
+    full_stop()
+    #rospy.signal_shutdown('Finished Docking')
+    listener_thread.join()
+    print('that\'s all folks, end of the story')
--- scripts/autonomous_docking_copy.py
+++ scripts/autonomous_docking_copy.py
@@ -1,3 +1,4 @@
+from logging import error
 from numpy.core.numeric import full
 from geometry_msgs import msg
 import rospy 
@@ -10,6 +11,11 @@ import matplotlib.pyplot as plt
 from subprocess import call, Popen
 import time
 
+# CONSTANTS
+OFFSET = 250
+MIN_ERROR = 10
+
+xavier_setup = 'export ROS_MASTER_URI=http://192.168.1.101:11311 && export ROS_IP=192.168.1.112'
 
 right_wheel_publisher = rospy.Publisher('right_velocity/command', Float64, queue_size=10)
 left_wheel_publisher = rospy.Publisher('left_velocity/command', Float64, queue_size=10)
@@ -17,48 +23,57 @@ left_wheel_publisher = rospy.Publisher('left_velocity/command', Float64, queue_s
 rospy.init_node('robot_controller', anonymous=True)
 
 #W miarę OK
-Kp = 7
+Kp = 14
 Ki = 7
-Kd = 0.03
+Kd = 0.3
 
 #Setpoint [rad/s]
+
+# RIGHT WHEEL PID
 pid_right = PID(Kp, Ki, Kd, setpoint=0)
 pid_right.sample_time = 0.01
+pid_right.output_limits = (-1000, 1000)
 
-pid_right.output_limits = (0, 1000)
-
+# LEFT WHEEL PID
 pid_left = PID(Kp, Ki, Kd, setpoint=0)
 pid_left.sample_time = 0.01
-pid_left.output_limits = (0, 1000)
+pid_left.output_limits = (-1000, 1000)
 
+# ALIGNMENT PID
+# pid_align = PID(1.5, 0.2, 0.008) # było 0.5, 0.1, 0.0
+pid_align = PID(0.5, 0.003, 0.007)
+pid_align.sample_time = 0.01
+pid_align.setpoint=0
+pid_align.output_limits = (-15, 15)
+
+# Read values from topic
 right_wheel_speed = 0
 left_wheel_speed = 0
 
-
-speed_values = {}
-speed_values['right'] = []
-speed_values['left'] = []
-
 rospy.init_node('robot_controller', anonymous=True)
 
-# PLOT
-fig, ax = plt.subplots()
-xdata, ydata = [], []
-ln, = plt.plot([], [], 'r-')
+tfmini_measurements = [0, 0, 0, 0]
+pololu_measurements = [0, 0, 0, 0]
 
-def init():
-    ax.set_xlim(-5, 5)
-    ax.set_ylim(-5, 5)
-    return ln,
+frame_size = 10
+average_tfmini = [[0 for i in range(frame_size)] for _ in range(4)]
 
-def update(frame):
-    global xdata, ydata
+def precise_tfmini_measurement():
+    precise_tfmini = [0, 0, 0, 0]
 
-    ln.set_data(xdata, ydata)
-    return ln,
+    for i in range(4):
+        average_tfmini[i].pop(0)
+        average_tfmini[i].append(tfmini_measurements[i])
+
+        for j in range(frame_size):
+            precise_tfmini[i] += average_tfmini[i][j]/frame_size
+            precise_tfmini[i] = int(precise_tfmini[i])
+
+    # print(precise_tfmini)
+    return precise_tfmini    
 
 def controller_manager_setup():
-    command = 'xavier_setup'
+    command = f'{xavier_setup}'
     command += ' && rosservice call /robot_driver/motor_contoller/turn_off_PID True'
     command += ' && rosrun controller_manager controller_manager stop diff_drive'
     command += ' && rosrun controller_manager controller_manager unload diff_drive'
@@ -67,33 +82,27 @@ def controller_manager_setup():
     p1=Popen(['/bin/bash', '-i', '-c', command])
     p1.wait()
 
+def callback_pololu(msg, i):
+    global pololu_measurements
+    pololu_measurements[i] = float(msg.range)
+    # print(pololu_measurements)
+
+def callback_tfmini(msg, i):
+    global tfmini_measurements
+    tfmini_measurements[i] = float(msg.range)
+    #print(tfmini_measurements)
 
 def callback_joint_state(msg):
     global right_wheel_speed, left_wheel_speed
     right_wheel_speed = msg.velocity[1]
     left_wheel_speed = msg.velocity[0]
-    print(right_wheel_speed, left_wheel_speed)
-
-def callback_stm_imu(msg):
-    global angular_velocity
-    angular_velocity = msg.angular_velocity.z
-    print
-
-def callback_pololu(msg, i):
-    print(i)
-
-def callback_tfmini(msg, i):
-    print(i)
-
+    # print(right_wheel_speed, left_wheel_speed)
 
 def listener():
-    # rospy.Subscriber('/joint_states', JointState, callback_joint_state)
-    # rospy.Subscriber('/stm_imu', Imu, callback_stm_imu)  
-
+    rospy.Subscriber('/joint_states', JointState, callback_joint_state)
     for i in range(4):
         rospy.Subscriber('/mega_driver/pololu/scan_'+str(i), Range, callback_pololu, callback_args=i)
-        rospy.Subscriber('/mega_driver/pololu/scan_'+str(i), Range, callback_pololu, callback_args=i)
-
+        rospy.Subscriber('/mega_driver/tfmini/scan_'+str(i), Range, callback_tfmini, callback_args=i)
     rospy.spin()
 
 def move_right_wheel(speed):
@@ -101,106 +110,78 @@ def move_right_wheel(speed):
 
     pid_right.setpoint=speed
     output = pid_right(right_wheel_speed)
+
+    if output > 0: output += OFFSET
+    elif output < 0: output -= OFFSET
+
     right_wheel_publisher.publish(output)
 
 def move_left_wheel(speed):
     global pid_left, left_wheel_speed 
-
     pid_left.setpoint=speed
     output = pid_left(left_wheel_speed)
+
+    if output > 0: output += OFFSET
+    elif output < 0: output -= OFFSET
+    
     left_wheel_publisher.publish(output)
-
-
-def move_forward(speed=10, distance=100):
-    global right_wheel_speed, left_wheel_speed
-    global speed_values
-
-    current_distance=0
-    t0 = rospy.Time.now().to_sec()
-    while current_distance < distance:
-        move_right_wheel(speed)
-        move_left_wheel(speed)
-        print(current_distance)
-        avg_speed = (right_wheel_speed+left_wheel_speed)/2
-
-        speed_values['right'].append(right_wheel_speed)
-        speed_values['left'].append(left_wheel_speed)
-
-        t1 = rospy.Time.now().to_sec()
-        current_distance = (t1-t0) * avg_speed
-
-    full_stop()
-
-
-def rotate(angle):
-    measured_angle=0
-
-    while measured_angle < angle:
-        t0 = rospy.Time.now().to_sec()
-        t1 = rospy.Time.now().to_sec()
-        # measured_angle += (t1-t0) * angular_velocity
-        measured_angle += (t1-t0) * 1
-        print(measured_angle)
-
-        if t1-t0 > 10:
-            break
 
 def full_stop():
     right_wheel_publisher.publish(0)
     left_wheel_publisher.publish(0)
 
-def move(velocity=25, distance=200):
-    global right_wheel_publisher, left_wheel_publisher  
-    global right_wheel_speed, left_wheel_speed
-    global pid_right
+def align_robot(sensor='tfmini', precision=False):
+    global pid_align
 
-    values = []
+    if sensor=='tfmini':
+        if precision:
+            precise_tfmini = precise_tfmini_measurement()
+            error = precise_tfmini[3] - precise_tfmini[2]
+        else:
+            error = tfmini_measurements[3] - tfmini_measurements[2]
+            
+        if -MIN_ERROR <= error <= MIN_ERROR: error = 0
 
-    listener_thread = threading.Thread(target=listener)
-    listener_thread.start()
+    elif sensor=='pololu':
+        error = pololu_measurements[3] - pololu_measurements[2] 
+        if -MIN_ERROR <= error <= MIN_ERROR: error = 0
 
-    right_wheel_publisher.publish(0)
-    current_distance=0
-    t0 = rospy.Time.now().to_sec()
+    
+    # print('tf2', tfmini_measurements[2], end='\t')
+    # print('tf3', tfmini_measurements[3], end='\t')
 
-    while current_distance < distance:
-        #Setpoint value using PID
-        print(right_wheel_speed)
-        values.append(right_wheel_speed)
-        pid_right.setpoint=velocity
-        
-        #Controll motor with PID
-        output = pid_right(right_wheel_speed)
+    pid_align.setpoint=0
+    output_align = pid_align(error)
 
-        #Set RW speed to the one given by PID
-        right_wheel_publisher.publish(output)
+    # if output_align > 0: output_align += 10
+    # elif output_align <0: output_align -= 10
 
-        t1 = rospy.Time.now().to_sec()
-        current_distance = (t1-t0)*velocity
-        print(current_distance)
+    # move_right_wheel(output_align)
 
-    right_wheel_publisher.publish(0)
-    left_wheel_publisher.publish(0)
-    listener_thread.join()
+    print(f'{output_align = }, {error=}')
 
-if __name__=='__main__':
+    return output_align
+
+
+if __name__ == '__main__':
 
     #Use right_velocity and left_velocity instead of diff_drive
     #Disconnect built in PID controller
-    # controller_manager_setup()
+    controller_manager_setup()
 
     #Start listener thread
     listener_thread = threading.Thread(target=listener)
     listener_thread.start()
 
-    # move_forward(10, 50)
-
-    # rotate(90)
-
-    # plt.plot(speed_values['right'])
-    # plt.plot(speed_values['left'])
-    # plt.show()
-
-    time.sleep(5)
-
-    listener_thread.join()
+    try:
+        while True:
+            # move_right_wheel(5+align_robot())
+            # move_left_wheel(5)
+            # time.sleep(0.01)
+            full_stop()
+            #right_wheel_publisher.publish(0)
+            #precise_tfmini_measurement()
+    
+    except KeyboardInterrupt:
+        full_stop()
+        listener_thread.join()
