817926b53a2b6e408443d26f1facb36cc94b9cd1
szymon <szymciem8@gmail.com>
2022-01-13 23:57:42 +0100

    data analysis images

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,7 +1,8 @@
+from codecs import ignore_errors
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
-import math
+import csv
 import numpy as np
 
 def get_angle(error):
@@ -9,7 +10,7 @@ def get_angle(error):
         Calculate angle of a robot against the wall
         '''
         D = 195
-        return math.asin(error/math.sqrt(D**2+error**2))
+        return np.arcsin(error/np.sqrt(D**2+error**2))
 
 def get_distance_from_wall(l1, l2, angle):
         '''
@@ -25,9 +26,11 @@ def get_distance_from_wall(l1, l2, angle):
         return d + np.cos(angle) * X0 - np.sin(angle) * Y0
 
 # path = os.path.join(os.path.dirname(__file__), 'logs/500/ride_0_base_speed_2.8_without_rosbag.csv')
-path = os.path.join(os.path.dirname(__file__), 'logs/500/test.csv')
+file = 'test'
+path = os.path.join(os.path.dirname(__file__), 'logs/500/' + file + '.csv')
 df = pd.read_csv(path)
 
+start_time = df.iloc[0,7]
 docking_time = df.iloc[-2,1]
 accurate_distance = df.iloc[-1,1]
 
@@ -37,12 +40,29 @@ df = df.iloc[:-2]
 df = df.apply(pd.to_numeric)
 
 df['distance[mm]'] = get_distance_from_wall(df['front[mm]'], df['rear[mm]'], df['angle[rad]'])
+df['time[s]'] = df['time[s]'] - start_time
+df = df[df['distance[mm]'] > 399.0]
 
-# df = df[df['distance[mm]'] != 150.0]
-print(df.head())
+df['angle[rad]'] = get_angle(df['error[mm]'])*1000
 
 ax = plt.gca()
 
 df.plot(kind='line', x='time[s]', y='distance[mm]', ax=ax)
+df.plot(kind='line', x='time[s]', y='angle[rad]', ax=ax)
+df.plot(kind='line', x='time[s]', y='PID Distance setpoint', ax=ax)
+df.plot(kind='line', x='time[s]', y='PID Align setpoint', ax=ax)
+
+
+plt.rcParams['figure.figsize'] = (5,5)
 
 plt.show()
+
+# path = os.path.join(os.path.dirname(__file__), 'images/from_logs/' + file + '.png')
+plt.savefig('images/from_logs/test.png')
+
+path = os.path.join(os.path.dirname(__file__), 'logs/new_500/' + file + '.csv')
+df.to_csv(path, index=False)
+
+with open(path, 'a') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Full Distance'] + [5] + list(np.zeros(6)))
